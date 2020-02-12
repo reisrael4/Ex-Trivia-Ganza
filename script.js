c229b5af247daf310fc52dbd1fdcc397113371e1
RachelIsrael <racheleisrael4@gmail.com>
2020-02-12 15:13:12 -0500

    all three difficulty levels functional and live

--- script.js
+++ script.js
@@ -1,98 +1,146 @@
 let random = document.querySelector('.random');
+let easy = document.querySelector('.easy');
+let medium = document.querySelector('.medium');
+let hard = document.querySelector('.hard');
 let questionBox = document.querySelector('.questionBox');
 let questionContainer = document.querySelector('.question');
 let answers = document.querySelectorAll('.answer');
 let modal = document.querySelector('.modal');
 let modalText = document.querySelector('.modalText');
 let nextButton = document.querySelector('.nextQuestion');
+let gameStatus = document.querySelector('.gameStatus');
+let gameOver = document.querySelector('.gameOver')
 let answersContentFinal = [];
 let i=0;
 let answersContent = [];
-let correctAnswer = [];
+let correctAnswer = document.querySelector('.correctAnswer')
 let questions = [];
-let randomUrl = 'https://opentdb.com/api.php?amount=10&type=multiple';
+let baseUrl = 'https://opentdb.com/api.php?amount=10&';
+let randomUrl = 'type=multiple';
+let easyUrl = 'difficulty=easy&type=multiple';
+let mediumUrl = 'difficulty=medium&type=multiple';
+let hardUrl = 'difficulty=hard&type=multiple';
 random.addEventListener('click', randomGame);
+easy.addEventListener('click', easyGame);
+medium.addEventListener('click', mediumGame);
+hard.addEventListener('click', hardGame);
 function randomGame(e){
     e.preventDefault();
-    fetch(randomUrl)
+    fetch(baseUrl+randomUrl)
         .then(res=>{
             return res.json()
         })
         .then(res=>{ 
             questions.push(...res.results);
-            // console.log(questions);
-            turn();
-            function turn(){
-                questionContainer.innerHTML = questions[i].question;
-                answersContent.push(questions[i]["correct_answer"], ...questions[i]["incorrect_answers"]);
-                correctAnswer.push(questions[i]["correct_answer"]);
-                // console.log(answersContent);
-                for(let j=0; j<answers.length; j++){
-                    answersContentFinal.push(...answersContent.splice(Math.floor(Math.random()*answersContent.length)));    
-                    answers[j].innerHTML = answersContentFinal[j]
-                }
-                // console.log(answersContentFinal)
-            }
-            answers.forEach(answer=>{
-                answer.addEventListener('click', checkCorrect)
-            })
-            function checkCorrect(e){
-                e.preventDefault();
-                i= i + 1;
-                console.log(i);
-                // console.log(e.target)
-                modal.classList.add('modalOpen')
-                if(e.target.innerHTML == correctAnswer[0]){
-                    modalText.innerHTML = "Good job!";
-                    modalText.style.color = 'green';
-                } else{
-                    modalText.innerHTML = "Nice try, but the correct answer was " + correctAnswer[0];
-                    modalText.style.color = 'red';
-                    }
-            }  
-                nextButton.addEventListener('click', newQuestion)
-                function newQuestion(e){
-                    e.preventDefault();
-                    modal.classList.add('modalClose');
-                    modal.addEventListener('animationend', closed);
-                    function closed(e){
-                        if(e.target == modal){
-                            modal.classList.remove('modalOpen');
-                            modal.classList.remove('modalClose');
-                            modal.removeEventListener('animationend', closed)
-                        }
-                    }
-                    correctAnswer = [];
-                    answersContentFinal = [];
-                    answersContent = [];
-                    turn();
-                }
             questionBox.classList.add('questionBoxOpen');
-            });
-            let gameStatus = document.querySelector('.gameStatus');
-            let gameOver = document.querySelector('.gameOver')
-                if(i===9){
-                    nextButton.style.display = 'none';
-                    gameOver.style.display = 'flex';
-                    gameStatus.innerHTML = "Game Over!";
-                    gameOver.addEventListener('click', endgame);
-                    function endgame(e){
-                        e.preventDefault();
-                        questionContainer.innerHTML = '';
-                        for(let j=0; j<answers.length; j++){
-                            answers[j].innerHTML = '';
-                        }
-                        questionBox.classList.remove('questionBoxOpen');
-                        modal.classList.add('modalClose')
-                        modal.addEventListener('animationend', closed);
-                        function closed(e){
-                            if(e.target == modal){
-                                modal.classList.remove('modalOpen');
-                                modal.classList.remove('modalClose');
-                                modal.removeEventListener('animationend', closed)
-                        }
-                    }
-                    }
-                }
+            game();
+            });            
+}
+function easyGame(e){
+    e.preventDefault();
+    fetch(baseUrl+easyUrl)
+        .then(res=>{
+            return res.json()
+        })
+        .then(res=>{ 
+            questions.push(...res.results);
+            questionBox.classList.add('questionBoxOpen');
+            game();
+            });            
+}
+function mediumGame(e){
+    e.preventDefault();
+    fetch(baseUrl+mediumUrl)
+        .then(res=>{
+            return res.json()
+        })
+        .then(res=>{ 
+            questions.push(...res.results);
+            questionBox.classList.add('questionBoxOpen');
+            game();
+            });            
+}
+function hardGame(e){
+    e.preventDefault();
+    fetch(baseUrl+hardUrl)
+        .then(res=>{
+            return res.json()
+        })
+        .then(res=>{ 
+            questions.push(...res.results);
+            questionBox.classList.add('questionBoxOpen');
+            game();
             
+            });            
+}
+function game(){
+    i=0;
+    turn();
+}
+function turn(){
+    console.log(i)
+    questionContainer.innerHTML = questions[i].question;
+    answersContent.push(questions[i]["correct_answer"], ...questions[i]["incorrect_answers"]);
+    correctAnswer.innerHTML = questions[i]["correct_answer"];
+    for(let j=0; j<answers.length; j++){
+        answersContentFinal.push(...answersContent.splice(Math.floor(Math.random()*answersContent.length)));    
+        answers[j].innerHTML = answersContentFinal[j]
+    }
+    answers.forEach(answer=>{
+        answer.addEventListener('click', checkCorrect)
+    })
+    nextButton.addEventListener('click', newQuestion)
+}
+function checkCorrect(e){
+    e.preventDefault();
+    modal.classList.add('modalOpen')
+    if(e.target.innerHTML == correctAnswer.innerHTML){
+        modalText.innerHTML = "Good job!";
+        modalText.style.color = 'green';
+    } else{
+        modalText.innerHTML = "Nice try, but the correct answer was " + correctAnswer.innerHTML;
+        modalText.style.color = 'red';
+        }
+}  
+function newQuestion(e){
+    e.preventDefault();
+    modal.classList.add('modalClose');
+    modal.addEventListener('animationend', closed);
+    answersContentFinal = [];
+    answersContent = [];
+    i += 1;
+    turn();
+    if(i===9){
+        nextButton.style.display = 'none';
+        gameOver.style.display = 'flex';
+        gameStatus.innerHTML = "Game Over!";
+        gameOver.addEventListener('click', endgame);
+    }
+
+}
+function endgame(e){
+    e.preventDefault();
+    questionContainer.innerHTML = '';
+    for(let j=0; j<answers.length; j++){
+        answers[j].innerHTML = '';
+    }
+    questionBox.classList.remove('questionBoxOpen');
+    modal.classList.add('modalClose')
+    modal.addEventListener('animationend', closed);
+    questions = [];
+    gameStatus.innerHTML = '';
+    gameOver.style.display = 'none';
+    nextButton.style.display = 'flex';
+    correctAnswer.innerHTML = '';
+    answersContentFinal = [];
+    answersContent = [];
+}
+function closed(e){
+    if(e.target == modal){
+        modal.classList.remove('modalOpen');
+        modal.classList.remove('modalClose');
+        modal.removeEventListener('animationend', closed)
+    }
+    answersContentFinal = [];
+    answersContent = [];
 }
